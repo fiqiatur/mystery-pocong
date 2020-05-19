6b0136aca653465db69a76e96f36094b0914f8e4
fiqiatur <fiqiatur.hrp@gmail.com>
2020-05-20 02:23:50 +0700

    Updates

---
+++ precache-manifest.e9540f1001a0ed7119accaae1a462790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11beefe845770239aa30c23c2e0d226a",
+    "url": "/mystery-pocong/index.html"
+  },
+  {
+    "revision": "90c51948974d72660133",
+    "url": "/mystery-pocong/static/css/main.149ecb9f.chunk.css"
+  },
+  {
+    "revision": "f7b84cd810271619eca9",
+    "url": "/mystery-pocong/static/js/2.7d94dc6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mystery-pocong/static/js/2.7d94dc6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90c51948974d72660133",
+    "url": "/mystery-pocong/static/js/main.2a9ab860.chunk.js"
+  },
+  {
+    "revision": "b096d3934a717cfebe32",
+    "url": "/mystery-pocong/static/js/runtime-main.4d9898a8.js"
+  }
+]);
